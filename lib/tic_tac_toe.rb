63a81bde69730312cddb0684a4fd02c23b06d7cd
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-10 06:31:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,7 +86,7 @@ def turn(board)
 end
 
 def won?(board)
-  winning_combo = nil 
+  winning_combo = nil
   x_win = false
   o_win = false
   WIN_COMBINATIONS.each do |combo|
@@ -97,12 +97,12 @@ def won?(board)
     o_win = combo.each do |space|
       board[space] == "O"
     end
-    if x_win || o_win 
+    if x_win || o_win
       winning_combo = combo
-    end 
+    end
   end
   if winning_combo != nil
-    return winning_combo 
+    return winning_combo
   else
     return false
   end
