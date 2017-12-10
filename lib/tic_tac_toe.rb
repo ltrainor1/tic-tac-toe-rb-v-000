9bf4f35c345c31d8c84541e5500fbd340c855936
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-10 05:57:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,17 @@
 # Helper Method
+board = new Array(9, " ")
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+
+def display_board(board)
+  puts" {board[0]} | {board[1]} | {board[2]} "
+  puts"-----------"
+  puts" {board[3]} | {board[4]} | {board[5]} "
+  puts"-----------"
+  puts" {board[6]} | {board[7]} | {board[8]} "
+end 
 
 # Define your WIN_COMBINATIONS constant
 
@@ -123,5 +133,10 @@ def winner(board)
     return board[value[0]]
   end
 end 
-   
+
+def input_to_index(input)
+  input = input.to_i 
+  index = input - 1
+  return index
+end 
   
